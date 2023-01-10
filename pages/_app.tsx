62c11ed3e246422bec302ce66fa18e3d8fa7da9c
Sebastian <rockmadero@gmail.com>
2023-01-10 17:32:11 -0500

    themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from "@next/font/google";
-import { createTheme, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { darkTheme, lightTheme } from '../themes';
 
 const roboto = Roboto({
   weight: ['300', '400', '700'],
   subsets: ['latin']
 })
 
-const basicTheme=createTheme();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ basicTheme }>
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline/>
       <main className={roboto.className}>
         <Component {...pageProps} />
       </main>
