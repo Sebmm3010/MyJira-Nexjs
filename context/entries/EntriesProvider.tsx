22866d4e2523ad8927d20cfb142a6507c0169874
Sebastian <rockmadero@gmail.com>
2023-01-12 15:09:37 -0500

    asigando interfaces

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,29 +1,52 @@
 import { FC, useReducer, ReactNode } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Entry } from '../../interfaces';
+
 import { EntriesContext, entriesReducer } from './';
 
 export interface EntriesState {
-    entries: [];
+    entries: Entry[];
 }
 
 
 const Entries_INITIAL_STATE: EntriesState = {
-    entries: [],
+    entries: [
+        {
+            _id: uuidv4(),
+            description: 'Ullamco excepteur cupidatat sint duis proident aliqua aliqua.',
+            status: 'pendiente',
+            createdAt: Date.now()
+        },
+        {
+            _id: uuidv4(),
+            description: 'Exercitation aliquip incididunt amet mollit enim ad commodo deserunt ea consectetur.',
+            status: 'pendiente',
+            createdAt: Date.now()
+        },
+        {
+            _id: uuidv4(),
+            description: 'Aute adipisicing culpa ullamco culpa excepteur duis sit non.',
+            status: 'pendiente',
+            createdAt: Date.now()
+        },
+    ],
 }
 
-interface Props{
-    children:ReactNode
+interface Props {
+    children: ReactNode
 }
 
 
-export const EntriesProvider:FC<Props> = ({ children }) => {
+export const EntriesProvider: FC<Props> = ({ children }) => {
 
-    const [state, dispatch] = useReducer( entriesReducer , Entries_INITIAL_STATE );
+    const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
     return (
         <EntriesContext.Provider value={{
             ...state
         }}>
-            { children }
+            {children}
         </EntriesContext.Provider>
     )
 };
