e5d733549fd1950f1d482d89bb1c175f0ec51796
Sebastian <rockmadero@gmail.com>
2023-01-12 14:16:20 -0500

    home maquetada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import { Typography } from "@mui/material";
+import { Card, CardHeader, Grid, Typography } from "@mui/material";
 import { NextPage } from "next";
 import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <Layout>
-        <Typography variant="h1" color='primary'>Hola mundo</Typography>
+      <Layout title="MyJira | Home">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='Pendientes' />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='En progreso' />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Card sx={{ height: 'calc(100vh - 100px)' }}>
+              <CardHeader title='Completado' />
+            </Card>
+          </Grid>
+        </Grid>
       </Layout>
     </>
   )
