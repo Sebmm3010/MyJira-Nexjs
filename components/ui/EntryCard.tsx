d1a4c3103ea2ee755cf2c51bafff84c7d539e679
Sebastian <rockmadero@gmail.com>
2023-01-13 14:00:31 -0500

    entris by status

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -1,6 +1,12 @@
 import { Card, CardActionArea, CardActions, CardContent, Typography } from "@mui/material"
+import { FC } from "react"
+import { Entry } from "../../interfaces"
 
-export const EntryCard = () => {
+interface Props{
+    entry:Entry
+}
+
+export const EntryCard:FC<Props> = ({ entry }) => {
     return (
         <Card
             sx={{ marginBottom: 1 }}
@@ -8,7 +14,7 @@ export const EntryCard = () => {
             <CardActionArea>
                 <CardContent>
                     <Typography sx={{ whiteSpace: 'pre-line' }}>
-                        Esto es la descripcion
+                        {entry.description}
                     </Typography>
                 </CardContent>
 
--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,22 +1,26 @@
+import { FC, useContext, useMemo } from "react";
 import { List, Paper } from "@mui/material"
+import { EntryStatus } from "../../interfaces";
 import { EntryCard } from './';
+import { EntriesContext } from '../../context/entries/EntriesContext';
 
-export const EntryList = () => {
+interface Props {
+    status: EntryStatus
+}
+
+export const EntryList: FC<Props> = ({ status }) => {
+
+    const { entries } = useContext(EntriesContext);
+
+    const entrisByStatus = useMemo(() => entries.filter(entry => entry.status === status), [entries]);
+
     return (
         <div>
-            <Paper sx={{ height: 'calc(100vh - 250px)', overflow: 'scroll', backgroundColor: 'transparent', '&::-webkit-scrollbar': { display: 'none' }, padding:'1px 8px'}}>
+            <Paper sx={{ height: 'calc(100vh - 250px)', overflow: 'scroll', backgroundColor: 'transparent', '&::-webkit-scrollbar': { display: 'none' }, padding: '1px 8px' }}>
                 <List sx={{ opacity: 1 }}>
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard />
-                    <EntryCard/>
+                    {entrisByStatus.map(entry => (
+                        <EntryCard key={entry._id} entry={entry}/>
+                    ))}
                 </List>
             </Paper>
         </div>
