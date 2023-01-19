32fe1755b312447aef6efcb3e75cd0b334f3b58c
Sebastian <rockmadero@gmail.com>
2023-01-19 16:00:38 -0500

    entries initial state []

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -11,26 +11,7 @@ export interface EntriesState {
 
 
 const Entries_INITIAL_STATE: EntriesState = {
-    entries: [
-        {
-            _id: uuidv4(),
-            description: 'Pendiente: Ullamco excepteur cupidatat sint duis proident aliqua aliqua.',
-            status: 'pendiente',
-            createdAt: Date.now()
-        },
-        {
-            _id: uuidv4(),
-            description: 'En Progreso: Exercitation aliquip incididunt amet mollit enim ad commodo deserunt ea consectetur.',
-            status: 'en-progreso',
-            createdAt: Date.now()
-        },
-        {
-            _id: uuidv4(),
-            description: 'Completado: Aute adipisicing culpa ullamco culpa excepteur duis sit non.',
-            status: 'completado',
-            createdAt: Date.now()
-        },
-    ],
+    entries: [],
 }
 
 interface Props {
