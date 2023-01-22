035ed55056abba158da5edc00f2e60298d5113b9
Sebastian <rockmadero@gmail.com>
2023-01-22 14:10:48 -0500

    manejo de form con useform

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,13 +1,25 @@
+import { ChangeEvent, useState } from 'react';
+
 import { capitalize, Grid, Button, Card, CardActions, CardContent, CardHeader, TextField, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, IconButton } from '@mui/material';
+
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 import { Layout } from "../../components/layouts";
 import { EntryStatus } from '../../interfaces/entry';
+import { useForm } from '../../hooks';
 
 const radioVariants: EntryStatus[] = ['pendiente', 'en-progreso', 'completado'];
 
 const EntriePage = () => {
+
+    const { inputValue, touched, setTouched, setInputValue, onInputChange } = useForm();
+
+    const [status, setStatus] = useState<EntryStatus>('pendiente');
+
+    const onStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setStatus(event.target.value as EntryStatus);
+    }
     return (
         <Layout title={'MyJira | Editar'}>
             <Grid
@@ -15,10 +27,10 @@ const EntriePage = () => {
                 justifyContent='center'
                 sx={{ marginTop: 2 }}
             >
-                <Grid xs={12} sm={8} md={6}>
+                <Grid item xs={12} sm={8} md={6}>
                     <Card>
                         <CardHeader
-                            title="Entrada: "
+                            title={`Entrada: ${inputValue}`}
                             subheader={`Creada hace minutos`}
                         />
                         <CardContent>
@@ -29,6 +41,8 @@ const EntriePage = () => {
                                 autoFocus
                                 multiline
                                 label='Nueva entrada'
+                                value={inputValue}
+                                onChange={onInputChange}
                             />
 
                             <FormControl>
@@ -38,6 +52,8 @@ const EntriePage = () => {
 
                                 <RadioGroup
                                     row
+                                    value={status}
+                                    onChange={onStatusChange}
                                 >
                                     {
                                         radioVariants.map(opt => (
