bb4a98a8e46086e9f41d7baaa6517d77675825f4
Sebastian <rockmadero@gmail.com>
2023-01-22 18:13:41 -0500

    cambios menores

--- database/db.ts
+++ database/db.ts
@@ -1,36 +1,37 @@
 import mongoose from 'mongoose';
 
-const mongooConection = {
+const mongoConection = {
     isConnected: 0
 }
 
 export const connect = async () => {
 
-    if (mongooConection.isConnected) {
+    if (mongoConection.isConnected) {
         console.log('Conectados con la base de datos');
         return;
     }
 
     if (mongoose.connections.length > 0) {
-        mongooConection.isConnected = mongoose.connections[0].readyState;
+        mongoConection.isConnected = mongoose.connections[0].readyState;
 
-        if (mongooConection.isConnected === 1) {
+        if (mongoConection.isConnected === 1) {
             console.log('usando conexion anterior');
             return;
         }
         await mongoose.disconnect();
     }
     await mongoose.connect(process.env.MONGO_URL || '');
-    mongooConection.isConnected = 1;
+    mongoConection.isConnected = 1;
     console.log('Conectado a mongoDB', process.env.MONGO_URL);
 }
 
 export const disconnect = async () => {
 
     if (process.env.NODE_ENV === 'development') return;
-    if (mongooConection.isConnected === 0) return;
+    if (mongoConection.isConnected === 0) return;
 
     await mongoose.disconnect();
+    mongoConection.isConnected=0
     console.log('Desconectado de MongoDB');
 
 
